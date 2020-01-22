7e43c8195c92963723d7de2922474f35b6a3901f
Mario <niemandmario@gmail.com>
2020-01-22 09:01:50 +0200

    nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
+import 'package:communityapp/shared/bottom_navy_bar.dart';
 import 'package:communityapp/screens/wrapper.dart';
 import 'package:communityapp/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:communityapp/shared/nav.dart';
 
 import 'models/user.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+
+      Map<int, Color> color =
+{
+  50:Color.fromRGBO(217,180,111, .1),
+  100:Color.fromRGBO(217,180,111, .2),
+  200:Color.fromRGBO(217,180,111, .3),
+  300:Color.fromRGBO(217,180,111, .4),
+  400:Color.fromRGBO(217,180,111, 1),//change .5 na 1 vir die login button fix
+  500:Color.fromRGBO(217,180,111, .6),
+  600:Color.fromRGBO(217,180,111, .7),
+  700:Color.fromRGBO(217,180,111, .8),
+  800:Color.fromRGBO(217,180,111, .9),
+  900:Color.fromRGBO(217,180,111, 1),
+};
+//waar die color func assign word
+MaterialColor colorCustom = MaterialColor(0xFFd9b46f, color);
+//color insert word
+const PrimaryColorTwo = const Color(0xFFd9b46f);
+
+class MyApp extends StatefulWidget {
+
+
+  
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
@@ -17,6 +46,47 @@ class MyApp extends StatelessWidget {
         home: Wrapper(),
       ),
     );
-     
-  }
+
+  int currentIndex = 0;
+  int _currentIndex = 0;
+
+     var colorCustom2 = colorCustom;
+          bottomNavigationBar: BottomNavyBar(
+             selectedIndex: _currentIndex,
+             showElevation: true,
+             itemCornerRadius: 8,
+             onItemSelected: (index) {
+               setState(() => 
+               _currentIndex = index);
+             } );
+             items: [
+               BottomNavyBarItem(
+                 icon: Icon(Icons.home),
+                 title: Text('Home'),
+                 activeColor: colorCustom,
+               ),
+               BottomNavyBarItem(
+                 icon: Icon(Icons.mail),
+                 title: Text('Messages'),
+                 activeColor: colorCustom,
+               ),
+               BottomNavyBarItem(
+                 icon: Icon(Icons.person),
+                 title: Text('Profile'),
+                 activeColor: colorCustom,
+               ),
+               BottomNavyBarItem(
+                 icon: Icon(Icons.report_problem),
+                 title: Text('Report'),
+                 activeColor: colorCustom,
+               ),
+               BottomNavyBarItem(
+                 icon: Icon(Icons.local_activity),
+                 title: Text('Facilities'),
+                 activeColor: colorCustom2,
+          ),
+        ];
+      
+    
+    }
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -6,16 +6,37 @@ import 'package:communityapp/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:communityapp/shared/bottom_navy_bar.dart';
+import 'package:communityapp/shared/nav.dart';
 
 class Home extends StatelessWidget {
 
   final AuthService _auth = AuthService();
 
+  
+      Map<int, Color> color =
+{
+  50:Color.fromRGBO(217,180,111, .1),
+  100:Color.fromRGBO(217,180,111, .2),
+  200:Color.fromRGBO(217,180,111, .3),
+  300:Color.fromRGBO(217,180,111, .4),
+  400:Color.fromRGBO(217,180,111, 1),//change .5 na 1 vir die login button fix
+  500:Color.fromRGBO(217,180,111, .6),
+  600:Color.fromRGBO(217,180,111, .7),
+  700:Color.fromRGBO(217,180,111, .8),
+  800:Color.fromRGBO(217,180,111, .9),
+  900:Color.fromRGBO(217,180,111, 1),
+};
+//waar die color func assign word
+MaterialColor colorCustom = MaterialColor(0xFFd9b46f, color);
+//color insert word
+const PrimaryColorTwo = const Color(0xFFd9b46f);
+
+
   @override
   Widget build(BuildContext context) {
     // StreamProvider<QuerySnapShot>.value(       //he used brew in stead of user
     //       value: DatabaseService().Booking,    
-    
+
 
     return Scaffold(
           backgroundColor: Colors.blue[50],
@@ -30,11 +51,51 @@ class Home extends StatelessWidget {
                 onPressed: ()async {
                    await _auth.signOut();
                 },
-                 //hier kom die nav bar
+                 
               )
             ],
           ),
           body: BookingList(),
+
+//hier kom die nav bar
+           int currentIndex = 0;
+  int _currentIndex = 0;
+
+     var colorCustom2 = colorCustom;
+          bottomNavigationBar: BottomNavyBar(
+             selectedIndex: _currentIndex,
+             showElevation: true,
+             itemCornerRadius: 8,
+             onItemSelected: (index) {
+               setState(() => 
+               _currentIndex = index);
+             } );
+             items: [
+               BottomNavyBarItem(
+                 icon: Icon(Icons.home),
+                 title: Text('Home'),
+                 activeColor: colorCustom,
+               ),
+               BottomNavyBarItem(
+                 icon: Icon(Icons.mail),
+                 title: Text('Messages'),
+                 activeColor: colorCustom,
+               ),
+               BottomNavyBarItem(
+                 icon: Icon(Icons.person),
+                 title: Text('Profile'),
+                 activeColor: colorCustom,
+               ),
+               BottomNavyBarItem(
+                 icon: Icon(Icons.report_problem),
+                 title: Text('Report'),
+                 activeColor: colorCustom,
+               ),
+               BottomNavyBarItem(
+                 icon: Icon(Icons.local_activity),
+                 title: Text('Facilities'),
+                 activeColor: colorCustom2,
+          ),
       );
   }
 }
